bb43a027da694de7d8da362c94b398c08aeacf3b
Ethereumistic <badjarovv@gmail.com>
2026-02-19 15:39:57 +0200

    better loading colors

--- components/experience/experience-gallery.tsx
+++ components/experience/experience-gallery.tsx
@@ -32,7 +32,7 @@ const ImageWithLoader = ({
     return (
         <>
             {isLoading && (
-                <div className="absolute inset-0 flex items-center justify-center bg-slate-100 z-10">
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-900 z-10">
                     <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
                 </div>
             )}
